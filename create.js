5b778de2a89b583e8530ad75f8f15e4bdf30542e
DORRA-cpu <dorrakrifa7@gmail.com>
2025-10-17 19:46:46 +0100

    Add files via upload

---
+++ create.js
@@ -0,0 +1,17 @@
+<script>
+  const fileInput = document.getElementById('file-upload');
+  const preview = document.getElementById('preview');
+
+  fileInput.addEventListener('change', function() 
+  {
+    const file = this.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.addEventListener('load', function() {
+        preview.setAttribute('src', this.result);
+        preview.style.display = 'block';
+      });
+      reader.readAsDataURL(file);
+    }
+  });
+</script>
